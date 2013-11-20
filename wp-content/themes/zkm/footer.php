6ea96b928429e16c3384084f3316f0e6e13779f0
jyubi2003 <jyubi2003@gmail.com>
2013-11-20 19:19:25 +0900

    20131104

--- wp-content/themes/zkm/footer.php
+++ wp-content/themes/zkm/footer.php
@@ -1,44 +1,16 @@
 <footer class="row">
-<p class="caution">※ ここからは footer.phpが管理しています</p>
-<div id="footerInner">
-<div class="large-12 columns">
-	<hr />
-		<div class="row">
-			<div class="large-6 columns">
-				<p>&copy; ZKM 全国健称マラソン会</p>
-			</div>
+	<!-- p class="caution">※ ここからは footer.phpが管理しています</p -->
+	<div id="footerInner">
+		<div class="large-12 columns">
+			<hr />
 		</div>
-</div>
 
-<?php
-/*
-ウィジットを表示します。functions.phpであらかじめ、次のように定義しています。
+		<div class="footer1">
+			<?php dynamic_sidebar('footer1'); ?>
+		</div>
 
-if ( function_exists('register_sidebar') ) {
-	register_sidebar(array(
-		'name' => 'フッター左',
-		'id' => 'footer1'
-	));
-	register_sidebar(array(
-		'name' => 'フッター右',
-		'id' => 'footer2'
-	));
-}
-
-ここで idに付加した「sidebar」を表示しています
-*/
-?>
-
-<div class="footer1">
-	<?php dynamic_sidebar('footer1'); ?>
-<!-- / .footer1 --></div>
-
-<!-- div class="footer2">
-	<?php dynamic_sidebar('footer2'); ?>
-<!-- / .footer2 --></div -->
-
-<small>&copy; <?php bloginfo('name'); ?></small>
-<!-- / #footerInner --></div>
+		<small>&copy; <?php bloginfo('name'); ?></small>
+	<!-- / #footerInner --></div>
 </footer>
 
 <script>
@@ -46,12 +18,10 @@ document.write('<script src=' +
 ('__proto__' in {} ? '/wp/_shared/js/vendor/zepto' : '/wp/_shared/js/vendor/jquery') +
 '.js><\/script>')
 </script>
-
 <script src="/wp/_shared/js/foundation.min.js"></script>
-
 <script>
 $(document).foundation();
 </script>
-<?php wp_footer(); ?>
+
 </body>
 </html>
--- wp-content/themes/zkm/functions.php
+++ wp-content/themes/zkm/functions.php
@@ -1,4 +1,27 @@
 <?php
+/*
+管理画面で「ウィジット」を使えるように設定します。
+sidebar.php で定義している「sidebar」を表示できるように設定します
+
+Lecture6-16を試す際は、先にこのスクリプトを削除してください
+*/
+if ( function_exists('register_sidebar') ) {
+	register_sidebar(array(
+		'name' => 'サイドバー',
+		'id' => 'sidebar'
+	));
+
+	register_sidebar(array(
+		'name' => 'フッター左',
+		'id' => 'footer1'
+	));
+	
+/*
+	register_sidebar(array(
+		'name' => 'フッター右',
+		'id' => 'footer2'
+	));
+
 if ( function_exists('register_sidebar') ) {
 	register_sidebar();
 
@@ -6,7 +29,7 @@ if ( function_exists('register_sidebar') ) {
 		'name' => 'インフォメーション',
 		'id' => 'footer1'
 	));
-/*
+
 	register_sidebar(array(
 		'name' => 'フッター右',
 		'id' => 'footer2'
@@ -14,4 +37,3 @@ if ( function_exists('register_sidebar') ) {
 */
 }
 ?>
-
